b186dafe454fa9b483d2f48506924c3819ecb24f
Suphakit P. <me@suphakit.net>
2025-11-12 22:27:38 +0700

    fix<module>: fix qr type error

--- src/Modules/qr-generate.ts
+++ src/Modules/qr-generate.ts
@@ -1,8 +1,24 @@
 import { generate } from "promptparse";
 
 export const genPayload = (target: string, value?: number | null) => {
+  var type: "MSISDN" | "NATID" | "EWALLETID" | "BANKACC" = "MSISDN";
+
+  switch (target.length) {
+    case 13:
+      type = "NATID";
+      break;
+
+    case 15:
+      type = "EWALLETID";
+      break;
+
+    default:
+      type = "MSISDN";
+      break;
+  }
+
   const payload = generate.anyId({
-    type: "NATID",
+    type: type,
     target: target,
     amount: value !== null && value !== undefined ? value : 0,
   });
